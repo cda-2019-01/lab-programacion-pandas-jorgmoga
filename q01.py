97d7d857e7448d9d6b294d37df80f1a6dd5fce73
Jorge Eliecer Montoya Gallo <50222297+jorgmoga@users.noreply.github.com>
2019-05-17 21:47:15 -0500

    Update q01.py

--- q01.py
+++ q01.py
@@ -2,3 +2,10 @@
 ## Imprima la cantidad de registros por cada letra 
 ## de la columna _c1 de la tabla tbl0
 ## 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+tb = pd.read_csv("tbl0.tsv", sep="\t")
+##tb1.head()
+tb.groupby('_c1').count()['_c0']
+tb.to_csv("tb1con.csv")
