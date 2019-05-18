34e16aa87b43ca20ffc15793341918834b2200c0
Jorge Eliecer Montoya Gallo <50222297+jorgmoga@users.noreply.github.com>
2019-05-18 06:15:31 -0500

    Update q06.py

--- q06.py
+++ q06.py
@@ -2,4 +2,10 @@
 ## Imprima la suma de la _c2 por cada letra de la _c1 
 ## de la tabla tbl0
 ## 
-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+tb = pd.read_csv("tbl0.tsv", sep="\t")
+##tb1.head()
+tb6=tb.groupby('_c1').sum()['_c2']
+print(tb6)
