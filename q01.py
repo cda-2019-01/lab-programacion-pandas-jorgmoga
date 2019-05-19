2a5b37f24fd0a1b19b92dbf0043cadb738f7c949
Jorge Eliecer Montoya Gallo <50222297+jorgmoga@users.noreply.github.com>
2019-05-19 13:04:53 -0500

    Update q01.py

--- q01.py
+++ q01.py
@@ -7,5 +7,5 @@ import numpy as np
 import matplotlib.pyplot as plt
 tb = pd.read_csv("tbl0.tsv", sep="\t")
 ##tb1.head()
-tb.groupby('_c1').count()['_c0']
-tb.to_csv("tb1con.csv")
+tb1=tb.groupby('_c1').count()['_c0']
+print(tb1)
