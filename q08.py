1dc7388088fe9ebc796ff3bbd15fd3e74304549f
Jorge Eliecer Montoya Gallo <50222297+jorgmoga@users.noreply.github.com>
2019-05-19 14:12:48 -0500

    Update q08.py

--- q08.py
+++ q08.py
@@ -5,6 +5,5 @@ import pandas as pd
 import numpy as np
 tb = pd.read_csv("tbl0.tsv", sep="\t")
 tb8=tb.copy()
-#tb8['ano']=tb8[year('_c3')]
-#tb8=tb8.assign(ano=tb8.values)
-#print(tb8)
+tb8['ano'] = tb8['_c3'].str.split('-').str[0]
+print(tb8)
