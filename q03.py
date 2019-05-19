27b43f25d04b4526a3dd0bc45cc8b7190ab08a70
Jorge Eliecer Montoya Gallo <50222297+jorgmoga@users.noreply.github.com>
2019-05-19 13:09:46 -0500

    Update q03.py

--- q03.py
+++ q03.py
@@ -5,6 +5,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-tb2 = pd.read_csv("tbl0.tsv", sep="\t")
+tb = pd.read_csv("tbl0.tsv", sep="\t")
 ##tb1.head()
-tb2.groupby('_c1').max()['_c2']
+tb3=tb.groupby('_c1').max()['_c2']
+print(tb3)
