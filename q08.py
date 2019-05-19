bd453f82ad16c061a8ebacee56721f2a2a36f9fa
Jorge Eliecer Montoya Gallo <50222297+jorgmoga@users.noreply.github.com>
2019-05-19 14:02:52 -0500

    Update q08.py

--- q08.py
+++ q08.py
@@ -1,4 +1,10 @@
 ##
 ## Agregue el año como una columna a la tabla tbl0.tsv 
 ## 
-
+import pandas as pd
+import numpy as np
+tb = pd.read_csv("tbl0.tsv", sep="\t")
+tb8=tb.copy()
+#tb8['ano']=tb8[year('_c3')]
+#tb8=tb8.assign(ano=tb8.values)
+#print(tb8)
