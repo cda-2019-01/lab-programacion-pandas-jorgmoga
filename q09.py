5ab9df10c17236bbb6708a4b1269973760155d73
Jorge Eliecer Montoya Gallo <50222297+jorgmoga@users.noreply.github.com>
2019-05-26 12:01:29 -0500

    Update q09.py

--- q09.py
+++ q09.py
@@ -7,8 +7,8 @@ import pandas as pd
 import numpy as np
 tb = pd.read_csv("tbl0.tsv", sep="\t")
 dfaux = tb.groupby('_c1')['_c2'].apply(list)
-df = pd.DataFrame()
-df['_c1'] = dfaux.keys()
-df['_c2'] = [elem for elem in dfaux]
-df['_c2'] = [":".join(str(v) for v in sorted(elem)) for elem in df['_c2']]
-print(df)
+tb9 = pd.DataFrame()
+tb9['_c1'] = dfaux.keys()
+tb9['_c2'] = [elem for elem in dfaux]
+tb9['_c2'] = [":".join(str(v) for v in sorted(elem)) for elem in tb9['_c2']]
+print(tb9)
