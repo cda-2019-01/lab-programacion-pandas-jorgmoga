2b664f1b21443c5062b5e40aab8959066457fe28
Jorge Eliecer Montoya Gallo <50222297+jorgmoga@users.noreply.github.com>
2019-05-26 12:04:54 -0500

    Update q10.py

--- q10.py
+++ q10.py
@@ -3,4 +3,12 @@
 ## separada por ',' de los valores de la columna _c4
 ## de la tabla tbl1.tsv
 ## 
-
+import pandas as pd
+import numpy as np
+tb = pd.read_csv("tbl1.tsv", sep="\t")
+dfaux = tb.groupby('_c0')['_c4'].apply(list)
+tb10 = pd.DataFrame()
+tb10['_c0'] = dfaux.keys()
+tb10['lista'] = [elem for elem in dfaux]
+tb10['lista'] = [",".join(str(v) for v in sorted(elem)) for elem in tb10['lista']]
+print(tb10)
